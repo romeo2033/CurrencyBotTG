e3829434f342f35dcb060d0301e3cf0579b00bb1
Romeo2033 <roman.pereponov15@gmail.com>
2024-11-14 18:17:08 +0300

    Lil upd

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 TOKEN = '7554425823:AAFDp_ZEbsJsJwW-S8OrKMKKdJBZc8xJ7Og' # Токен телеграм-бота, полученный у BotFather, бот: @curconv_skillfactory_bot
 API_key = '7e26f130e8e501fcc725a260' # API ключ для exchangerate-api.com
 
-# Список валют и их тикеров
+# Словарь валют и их тикеров
 currencies = {
     'Рубль':'RUB',
     'Доллар':'USD',
     'Евро':'EUR'
 }
 
+# Словарь валют и их знаков
 signs = {
     'Рубль':'₽',
     'Доллар':'$',
